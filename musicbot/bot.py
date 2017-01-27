cdfbeb0402af9cfa7e1e4d5c9b85e21325b2bfe8
Geoffrey Westhoff <geoffcoproductions@gmail.com>
2017-01-27 09:34:43 +0100

    -added ;q alias voor ;queue

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -1550,7 +1550,58 @@ class MusicBot(discord.Client):
 
         message = '\n'.join(lines)
         return Response(message, delete_after=30)
+        
 
+
+    async def cmd_q(self, channel, player):
+        """
+        Usage:
+            {command_prefix}queue
+
+        Prints the current song queue.
+        """
+
+        lines = []
+        unlisted = 0
+        andmoretext = '* ... and %s more*' % ('x' * len(player.playlist.entries))
+
+        if player.current_entry:
+            song_progress = str(timedelta(seconds=player.progress)).lstrip('0').lstrip(':')
+            song_total = str(timedelta(seconds=player.current_entry.duration)).lstrip('0').lstrip(':')
+            prog_str = '`[%s/%s]`' % (song_progress, song_total)
+
+            if player.current_entry.meta.get('channel', False) and player.current_entry.meta.get('author', False):
+                lines.append("Now Playing: **%s** added by **%s** %s\n" % (
+                    player.current_entry.title, player.current_entry.meta['author'].name, prog_str))
+            else:
+                lines.append("Now Playing: **%s** %s\n" % (player.current_entry.title, prog_str))
+
+        for i, item in enumerate(player.playlist, 1):
+            if item.meta.get('channel', False) and item.meta.get('author', False):
+                nextline = '`{}.` **{}** added by **{}**'.format(i, item.title, item.meta['author'].name).strip()
+            else:
+                nextline = '`{}.` **{}**'.format(i, item.title).strip()
+
+            currentlinesum = sum(len(x) + 1 for x in lines)  # +1 is for newline char
+
+            if currentlinesum + len(nextline) + len(andmoretext) > DISCORD_MSG_CHAR_LIMIT:
+                if currentlinesum + len(andmoretext):
+                    unlisted += 1
+                    continue
+
+            lines.append(nextline)
+
+        if unlisted:
+            lines.append('\n*... and %s more*' % unlisted)
+
+        if not lines:
+            lines.append(
+                'There are no songs queued! Queue something with {}play.'.format(self.config.command_prefix))
+
+        message = '\n'.join(lines)
+        return Response(message, delete_after=30)   	
+	
+		
     async def cmd_clean(self, message, channel, server, author, search_range=50):
         """
         Usage:
@@ -1565,7 +1616,7 @@ class MusicBot(discord.Client):
         except:
             return Response("enter a number.  NUMBER.  That means digits.  `15`.  Etc.", reply=True, delete_after=8)
 
-        await self.safe_delete_message(message, quiet=True)
+        await self.safe_delete_message(message, quiet=True)	
 
         def is_possible_command_invoke(entry):
             valid_call = any(
